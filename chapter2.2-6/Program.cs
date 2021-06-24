f8d53d25649de969c99cbb6cb713b383b89f1c2d
username <1151555861@qq.com>
2021-06-24 13:50:03 +0800

    first commit

---
+++ chapter2.2-6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace chapter2._2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           PrintItems<string>(List<string> ,CultureInfo.InvariantCulture) ;//不知道主函数怎么写
+        }
+        static void PrintItems<T>(List<T> items) where T : IFormattable
+        {
+              CultureInfo culture = CultureInfo.InvariantCulture;
+              foreach (T item in items)
+            {
+                Console.WriteLine(item.ToString(null, culture));
+            }
+        }
+    }
+}
